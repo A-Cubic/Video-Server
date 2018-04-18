c702b1b3a2a475a7fb90424611d2a5a2ab26064a
mitoriko <mitoriko@icloud.com>
2018-04-19 00:07:48 +0800

    清理原测试方法

--- Video-Server/Json/JsonObjs.cs
+++ Video-Server/Json/JsonObjs.cs
@@ -12,38 +12,6 @@ namespace VideoServer.Json
         public string msg;
     }
 
-
-    /// <summary>
-    /// 列表页列表信息
-    /// </summary>
-    public class VListItem
-    {
-        public string id;
-        public string img;
-        public string name;
-        public string time;
-        public string person;
-    }
-
-    /// <summary>
-    /// 列表页题头信息
-    /// </summary>
-    public class VListMain
-    {
-        public string img;
-        public string word;
-        public string text;
-    }
-
-    /// <summary>
-    /// 列表页信息
-    /// </summary>
-    public class VList
-    {
-        public List<VListItem> item;
-        public VListMain main;
-    }
-
     /// <summary>
     /// 首页信息
     /// </summary>
